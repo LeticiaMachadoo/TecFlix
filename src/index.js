c43c1b1d77c9438b430a1252ee261771560e8f7e
Leticia Machado <leticiamachado96@outlook.com>
2020-08-01 15:21:57 -0300

    Adicionando react-router

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
+import Home from './pages/Home';
+import CadastroVideo from './pages/cadastro/Video';
+import CadastroCategoria from './pages/cadastro/Categoria';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+  <Switch>
+  <Route path='/' component={Home} exact />
+  <Route path='/cadastro/video' component={CadastroVideo} exact />
+  <Route path='/cadastro/categoria' component={CadastroCategoria} exact />
+  <Route  component={() => (<>Página 404</>)}/>
+  </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
