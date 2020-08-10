40b40f00b8934707cafc5e39f29cc844c2483aba
Leticia Machado <leticiamachado96@outlook.com>
2020-08-09 23:32:32 -0300

    add mock server

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -30,7 +30,9 @@ function CadastroCategoria() {
 
   useEffect(() => {
     if (window.location.href.includes('localhost')) {
-      const URL = 'http://localhost:8080/categorias';
+      const URL = window.location.hostname.includes('localhost')
+        ? 'http://localhost:8080/categorias'
+        : 'https://tecflix-project.herokuapp.com/categorias';
       fetch(URL)
         .then(async (response) => {
           if (response.ok) {
